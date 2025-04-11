6aee7e516f5d534ba3eb1cd512505304359fbb1f
fayaazm1 <fayaazm1@umbc.edu>
2025-04-11 01:21:00 -0400

    Switched to HashRouter for better static hosting support

--- lost-and-found/src/main.jsx
+++ lost-and-found/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
 import App from './App';
@@ -8,9 +8,9 @@ import App from './App';
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   </React.StrictMode>
 );
